c360295351720865ed40e7f2c2fe2f034ac307c6
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-05-23 18:37:03 -0300

    se realiza el maquetado y la carga logica de Formulario tarea

---
+++ src/components/FormularioTarea.jsx
@@ -0,0 +1,34 @@
+import { Form, Button } from "react-bootstrap";
+import ListaTareas from "./ListaTareas";
+import { useState } from "react";
+
+const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTareas([...tareas, tarea]);
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex" controlId="tarea">
+          <Form.Control
+            type="text"
+            placeholder="ingresar una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+          <Button variant="primary" type="submit">
+            Agregar
+          </Button>
+        </Form.Group>
+      </Form>
+      <ListaTareas></ListaTareas>
+    </>
+  );
+};
+
+export default FormularioTarea;
