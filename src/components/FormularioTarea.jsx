7fee04de21acffeb9a7b80966cfd928906dd4350
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-05-30 21:45:54 -0300

    se realizan los cambios para que el ejercicio quede acorde al enunciado

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,6 +1,6 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const FormularioTarea = () => {
   const [tarea, setTarea] = useState("");
